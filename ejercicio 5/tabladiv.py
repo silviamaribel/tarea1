6613f4e3dd9cf5add33e7e73fb65b4685a869a66
Silvia <silvia@hotmail.com>
2014-09-05 01:07:15 +0200

    mi quinto commit

---
+++ ejercicio 5/tabladiv.py
@@ -0,0 +1,15 @@
+a=1
+b=1
+con=1
+d=1
+print("abajo se muestra la tabla de la division del 1 al 10")
+while con<=10:
+	while d<=10:
+		c=b/a
+		print c
+		b=b+con
+		d=d+1
+	print("------------------")
+	con=con+1
+	b=con
+	d=1
